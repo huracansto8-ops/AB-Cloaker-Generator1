0b2882dd387e8bd13c78cbf0082683b887d08959
huracansto8-ops <huracansto8@gmail.com>
2025-10-24 09:49:41 -0700

    Change game URL from warbrokers.io to lordz-io.com

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function opengame() {
 	var win = window.open()   
-	var url = "https://warbrokers.io" 
+	var url = "https://lordz-io.com" 
 	var iframe = win.document.createElement('iframe')
 	iframe.style.width = "100%";
 	iframe.style.height = "100%";
