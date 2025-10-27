e1bbe5d422f563f7d4443e3747c373ef868241b4
huracansto8-ops <huracansto8@gmail.com>
2025-10-27 09:53:02 -0700

    Change game URL in opengame function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function opengame() {
 	var win = window.open()   
-	var url = "https://ethonion10.neocities.org" 
+	var url = "https://www.kodub.com" 
 	var iframe = win.document.createElement('iframe')
 	iframe.style.width = "100%";
 	iframe.style.height = "100%";
