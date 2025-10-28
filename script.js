3e0a34639af8d151dfd73431749a672cc94ae697
huracansto8-ops <huracansto8@gmail.com>
2025-10-28 15:10:50 -0700

    Update game URL to include '/ships3d'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function opengame() {
 	var win = window.open()   
-	var url = "https://yp3d.com" 
+	var url = "https://yp3d.com/ships3d" 
 	var iframe = win.document.createElement('iframe')
 	iframe.style.width = "100%";
 	iframe.style.height = "100%";
